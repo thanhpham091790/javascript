25b5cf6d09a3a61d387a4812077d357193276b45
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-09-22 17:53:57 -0600

    Javascript function apply()

---
+++ 01-js-functions/05-function-this.js
@@ -0,0 +1,27 @@
+// Function call() method
+const person = {
+    fullName: function (age) {
+        return age == undefined ? `${this.firstName} ${this.lastName}` : `${this.firstName} ${this.lastName} is ${age} years old.`;
+    }
+};
+const person2 = {
+    firstName: 'Thanh',
+    lastName: 'Pham'
+}
+const person3 = {
+    firstName: 'Toan',
+    lastName: 'Pham'
+}
+console.log(person.fullName.call(person2));
+console.log(person.fullName.call(person3));
+console.log(person.fullName.call(person2, 35));
+console.log(person.fullName.call(person3, 32));
+
+// Function apply() method
+// The different between call() and apply() method is: call() takes arguments separately by comma, and apply() takes arguments as an array.
+const person4 = {
+    firstName: 'Ngan',
+    lastName: 'Do'
+}
+console.log(person.fullName.apply(person4));
+console.log(person.fullName.apply(person4, [26]));
