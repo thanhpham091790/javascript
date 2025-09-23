87098e0068c3db9e785ab1a0e649996eed1d40ba
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-09-22 19:32:40 -0600

    Create an new object using Object.assign() method.

--- 02-js-objects/01-object-definitions.js
+++ 02-js-objects/01-object-definitions.js
@@ -16,10 +16,26 @@ const person2 = new Object({
 });
 console.log(`${person2.firstName} is ${person2.age} years old.`);
 
-// Using fromEntries()
+// Using Object.fromEntries() method.
 const fruits = [
     ['apple', 300],
     ['pears', 900],
     ['bananas', 500]
 ];
 console.log(Object.fromEntries(fruits));
+
+// Using Object.assign() method.
+// Create a new object by copying properties from one or more source objects.
+// If they have some common properties, the properties of later object will overwrite the properties of previous object.
+const person3 = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 50,
+    eyeColor: 'brown'
+}
+
+const person4 = {
+    firstName: 'Anne',
+    lastName: 'Smith'
+}
+console.log(Object.assign(person3, person4));
