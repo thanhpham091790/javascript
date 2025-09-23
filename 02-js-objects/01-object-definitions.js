7721d02ae05890b522f9183d42f109f7de61e99d
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-09-22 18:56:02 -0600

    Create an object using object literal.

---
+++ 02-js-objects/01-object-definitions.js
@@ -0,0 +1,10 @@
+// Using an object literal
+const person1 = {
+    firstName: 'Thanh',
+    lastName: 'Pham',
+    age: 35,
+    eyeColor: 'brown'
+}
+console.log(`${person1.firstName} is ${person1.age} years old.`);
+
+
