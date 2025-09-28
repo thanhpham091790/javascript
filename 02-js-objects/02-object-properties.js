781a17ea85fe67f0458ccad4ba649f2c3e778a9f
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-09-28 06:50:15 -0600

    Property default values.

--- 02-js-objects/02-object-properties.js
+++ 02-js-objects/02-object-properties.js
@@ -14,3 +14,15 @@ console.log(`${person1[firstName]} is ${person1[age]} years old.`);
 // Adding New Properties
 person1.nationality = 'English';
 console.log(person1['nationality']);
+
+// Property default values
+function Person1(first, last, age, eye) {
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.eye = eye;
+    this.nationality = 'Vietnamese';
+}
+const myFather = new Person1('Tam', 'Pham', 60, 'brown');
+const myMother = new Person1('Thao', 'Bui', 57, 'brown');
+console.log(`My father is ${myFather.nationality}. My mother is ${myMother.nationality}.`);
