0852042ac3001d95f82c91ed1cea435ed275d735
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-09-30 07:38:25 -0600

    To add a new property to constructor function, must add it to the constructor function prototype.

--- 02-js-objects/05-object-constructors.js
+++ 02-js-objects/05-object-constructors.js
@@ -22,5 +22,18 @@ const myMother1 = new Person1('Thao', 'Bui', 57, 'brown');
 document.querySelector('#demo').innerHTML = `My father is ${myFather1.age} years old, and my mother is ${myMother1.age} years old.`;
 
 // Adding a property to an instant of object type.
-myFather1.car = 'Honda';
-document.querySelector('#demo').innerHTML = `My father car is ${myFather1.car}.`;
+function Person2(first, last, age, eye) {
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.eyeColor = eye;
+}
+const myFather2 = new Person2('Tam', 'Pham', 60, 'brown');
+myFather2.car = 'Honda';
+document.querySelector('#demo').innerHTML = `My father car is ${myFather2.car}.`;
+
+// Adding a property to a constructor
+Person2.car = 'Honda';
+Person2.prototype.car = 'Honda';
+const myMother2 = new Person2('Thao', 'Bui', 57, 'brown');
+document.querySelector('#demo').innerHTML = `My mother car is ${myMother2.car}.`;
