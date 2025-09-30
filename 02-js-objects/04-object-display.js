4c71195a6fc21758f6d426c51e863754526c0869
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-09-30 00:35:54 -0600

    Displaying the object using Object.values() method.

--- 02-js-objects/04-object-display.js
+++ 02-js-objects/04-object-display.js
@@ -6,8 +6,19 @@ const person1 = {
 document.querySelector('#demo').innerHTML = person1;
 
 // Displaying the object properties by name
-let text = `${person1.name}, ${person1.age}, ${person1.city}.`;
-document.querySelector('#demo').innerHTML = text;
+let text1 = `${person1.name}, ${person1.age}, ${person1.city}.`;
+document.querySelector('#demo').innerHTML = text1;
+
 // Displaying the object properties in a loop
+let text2 = '';
+for (let x in person1) {
+    text2 += person1[x] + ' ';
+}
+document.querySelector('#demo').innerHTML = text2;
+
 // Displaying the object using object.values()
+const values = Object.values(person1);
+let text3 = values.toString();
+document.querySelector('#demo').innerHTML = text3;
+
 // Displaying the object using JSON.stringify()
