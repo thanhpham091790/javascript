2879f87c198b1628df817111aeb14bc6e40bcfd5
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-09-30 07:21:48 -0600

    A value given to a property will be a default value for all objects created by the constructor.

--- 02-js-objects/05-object-constructors.js
+++ 02-js-objects/05-object-constructors.js
@@ -7,3 +7,16 @@ function Person(first, last, age, eye) {
 }
 const myFather = new Person('Tam', 'Pham', 60, 'brown');
 document.querySelector('#demo').innerHTML = `My father is ${myFather.age} years old.`;
+
+// Property default values
+function Person1(first, last, age, eye) {
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.eyeColor = eye;
+    this.nationality = 'Vietnam';
+}
+
+const myFather1 = new Person1('Tam', 'Pham', 60, 'brown');
+const myMother1 = new Person1('Thao', 'Bui', 57, 'brown');
+document.querySelector('#demo').innerHTML = `My father is ${myFather1.age} years old, and my mother is ${myMother1.age} years old.`;
