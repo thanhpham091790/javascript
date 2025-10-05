46cdff2ea666653555d782c9c9afbbd07bdcb14a
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-10-04 21:58:34 -0600

    For potentially missing properties we can set default values.

--- 02-js-objects/07-object-destructuring.js
+++ 02-js-objects/07-object-destructuring.js
@@ -2,9 +2,18 @@ const demoEl = document.querySelector('#demo');
 
 // Object Destructuring
 const person1 = {
-    firstName: 'John',
-    lastName: 'Doe',
-    age: 50
+    firstName1: 'John',
+    lastName1: 'Doe',
+    age1: 50
 };
-let { lastName, firstName } = person1;
-demoEl.innerHTML = `${firstName} ${lastName}`;
+let { lastName1, firstName1 } = person1;
+demoEl.innerHTML = `${firstName1} ${lastName1}`;
+
+// Object default values
+const person2 = {
+    firstName2: 'Thanh',
+    lastName2: 'Pham',
+    age2: 35
+};
+let { lastName2, firstName2, country2 = 'VN' } = person2;
+demoEl.innerHTML = `${firstName2} ${lastName2} is from ${country2}`;
