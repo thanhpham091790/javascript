cb6c0b38956b839f5297b5c3790c740f5e98159d
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-10-11 22:19:22 -0600

    Since arrays are objects, arrays can be prevented from extensions too.

--- 02-js-objects/12-object-protection.js
+++ 02-js-objects/12-object-protection.js
@@ -6,3 +6,8 @@ const person1 = { firstName: 'Thanh', lastName: 'Pham' };
 Object.preventExtensions(person1);
 person1.nationality = 'Vietnam';
 demoEl.innerHTML = person1.nationality;
+
+const fruits1 = ["Banana", "Orange", "Apple", "Mango"];
+Object.preventExtensions(fruits1);
+fruits1.push('Kiwi');
+console.log(fruits1);
