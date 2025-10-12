63d8b4e6f4f43d0191f2e6ff3c0228de243188f6
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-10-11 22:32:05 -0600

    The Object.isExtensible() method returns true if an object or array is extensible.

--- 02-js-objects/12-object-protection.js
+++ 02-js-objects/12-object-protection.js
@@ -7,6 +7,10 @@ Object.preventExtensions(person1);
 person1.nationality = 'Vietnam';
 demoEl.innerHTML = person1.nationality;
 
+const fruits1 = ["Banana", "Orange", "Apple", "Mango"];
+Object.preventExtensions(fruits1);
+
 // Javascript Object.isExtensible() method
-let answer = Object.isExtensible(person1);
-console.log(answer);
+let isObjectExtensible = Object.isExtensible(person1);
+let isArrayExtensible = Object.isExtensible(fruits1);
+console.log(isObjectExtensible, isArrayExtensible);
