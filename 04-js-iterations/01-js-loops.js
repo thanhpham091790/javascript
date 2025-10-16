6cb4074808a006999d7ed72a3acb549c01eed3f1
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-10-16 17:29:22 -0600

    The for...of loop iterates over the values of iterable objects such as arrays, string, maps, sets, and nodelists.

--- 04-js-iterations/01-js-loops.js
+++ 04-js-iterations/01-js-loops.js
@@ -5,3 +5,16 @@ for (let key in person1) {
     text += person1[key] + ' ';
 }
 console.log(text);
+
+// The for...of loop
+const persons = [
+    { fname: 'Thanh', lname: 'Pham', age: 35 },
+    { fname: 'Toan', lname: 'Pham', age: 30 },
+    { fname: 'Tam', lname: 'Pham', age: 57 }
+];
+
+let firstNames = '';
+for (let person of persons) {
+    firstNames += person.fname + ' ';
+}
+console.log(firstNames);
