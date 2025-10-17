aa35a72fabe18473b77b115c48eaa127fe82bfc0
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-10-17 01:26:47 -0600

    Iterating over a set.

--- 04-js-iterations/02-js-iterables.js
+++ 04-js-iterations/02-js-iterables.js
@@ -5,7 +5,13 @@ for (const c1 of word1) {
 }
 
 // Iterating over an array
-const letters = ['a', 'b', 'c'];
-for (const c2 of letters) {
+const letters2 = ['a', 'b', 'c'];
+for (const c2 of letters2) {
     console.log(c2);
+}
+
+// Iterating over a set
+const letters3 = new Set(['a', 'b', 'c']);
+for (const c3 of letters3) {
+    console.log(c3);
 }
