48e7ec56ac9d017b67b3f1dba39eb2f6c3a32925
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-10-21 08:28:53 -0600

    When using the Javascript function setInterval(), you can specify a callback function to be executed for each interval.

--- 05-js-promises/02-js-asynchronous.js
+++ 05-js-promises/02-js-asynchronous.js
@@ -1,7 +1,14 @@
 const demoEl = document.querySelector('#demo');
 
 // Waithing for a Timeout
-setTimeout(myFunction1, 3000);
+setTimeout(myFunction1, 2000);
 function myFunction1() {
     demoEl.innerHTML = 'I love you!';
+}
+
+// Waiting for intervals
+setInterval(myFunction2, 4000);
+function myFunction2() {
+    let d = new Date();
+    demoEl.innerHTML = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
 }
