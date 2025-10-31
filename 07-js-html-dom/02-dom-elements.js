214fee3f7af478f5f5c6f9302da1581184545fc9
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-10-30 22:46:49 -0600

    We can access individual elements within an HTMLCollection by their zero-based index, similar to an array.

--- 07-js-html-dom/02-dom-elements.js
+++ 07-js-html-dom/02-dom-elements.js
@@ -1,3 +1,10 @@
-// Finding HTML Element by Id
-const element = document.getElementById('intro');
-document.getElementById('demo').innerHTML = 'The text from the intro paragraph is: ' + element.innerHTML;
+
+// Finding HTML elements by tag name.
+const elements = document.getElementsByTagName('p');
+console.log(elements);
+document.getElementById('demo').innerHTML =
+    'The text in first paragraph (index 0) is: ' + elements[0].innerHTML +
+    ' The text in second paragraph (index 1) is: ' + elements[1].innerHTML;
+
+
+
