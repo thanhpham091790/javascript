f389257e5d45c5514aaabf02d02e30151112e8c4
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-10-31 00:15:11 -0600

    Finding HTML elements by class name.

--- 07-js-html-dom/02-dom-elements.js
+++ 07-js-html-dom/02-dom-elements.js
@@ -1,10 +1,3 @@
-
-// Finding HTML elements by tag name.
-const elements = document.getElementsByTagName('p');
-console.log(elements);
+const elements = document.getElementsByClassName('intro');
 document.getElementById('demo').innerHTML =
-    'The text in first paragraph (index 0) is: ' + elements.namedItem('para1').innerHTML +
-    ' The text in second paragraph (index 1) is: ' + elements.namedItem('para2').innerHTML;
-
-const elementArray = Array.from(elements);
-console.log(elementArray[0].innerHTML);
+    'The second paragraph with class="intro" is: ' + elements.item(1).innerHTML;
