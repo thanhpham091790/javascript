c99279a3e8dff95ec2267900740ee353d526e47a
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-10-30 23:00:06 -0600

    Using the namedItem() method for elements with id or name attributes.

--- 07-js-html-dom/02-dom-elements.js
+++ 07-js-html-dom/02-dom-elements.js
@@ -3,8 +3,8 @@
 const elements = document.getElementsByTagName('p');
 console.log(elements);
 document.getElementById('demo').innerHTML =
-    'The text in first paragraph (index 0) is: ' + elements.item(0).innerHTML +
-    ' The text in second paragraph (index 1) is: ' + elements.item(1).innerHTML;
+    'The text in first paragraph (index 0) is: ' + elements.namedItem('para1').innerHTML +
+    ' The text in second paragraph (index 1) is: ' + elements.namedItem('para2').innerHTML;
 
 
 
