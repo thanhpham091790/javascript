eacdb5610e30564b9530c8ad07b3bac0b8387527
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-10-31 00:49:16 -0600

    Accessing form data by using FormData() and Object.fromEntries() method.

--- 07-js-html-dom/02-dom-elements.js
+++ 07-js-html-dom/02-dom-elements.js
@@ -1,6 +1,7 @@
-const form = document.forms['frm1'];
-let text = '';
-for (let i = 0; i < form.length; i++) {
-    text += form[i].value + '<br>';
-}
-document.querySelector('#demo').innerHTML = text;
+const form = document.querySelector('#frm1');
+document.addEventListener('submit', function (event) {
+    event.preventDefault();
+    const formData = new FormData(form);
+    const objectData = Object.fromEntries(formData);
+    console.log(objectData);
+});
