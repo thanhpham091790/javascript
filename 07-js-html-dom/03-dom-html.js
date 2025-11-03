a6dbf9b7c897f110789db6425367c4500c6d6eea
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-11-03 11:02:38 -0700

    Changing the value of an attribute.

--- 07-js-html-dom/03-dom-html.js
+++ 07-js-html-dom/03-dom-html.js
@@ -1,2 +1,5 @@
-// Changing HTML Content
-document.querySelector('#p1').innerHTML = 'New text!';;
+
+// Changing the value of an attribute
+const image = document.querySelector('#image');
+image.src = 'https://www.w3schools.com/js/landscape.jpg';
+console.log(image);
