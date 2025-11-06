d1cd642c7a575d554ed7a9bbc414f0ee5dc7c8af
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-11-06 10:59:40 -0700

    Assign events using the HTML DOM.

--- 07-js-html-dom/07-dom-events.js
+++ 07-js-html-dom/07-dom-events.js
@@ -1,4 +1,11 @@
-// Reacting to events
-function changeText(el) {
-    el.innerHTML = 'Ooop!';
+// Assign events using the HTML DOM
+const btn = document.querySelector('button');
+const demoEl = document.querySelector('#demo');
+console.log(btn);
+btn.onclick = () => {
+    displayDate('Thanh')
+};
+
+function displayDate(name) {
+    demoEl.innerHTML = name + ' ' + Date();
 }
