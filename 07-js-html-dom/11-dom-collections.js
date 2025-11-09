39521501f313005adb606b693095928d1bc35c65
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-11-09 09:04:10 -0700

    An HTMLCollection is not array. It may look like an array, but it not. You can loop through the list and refer to the elements with a number (just like an array). However, you can not use array methods.

--- 07-js-html-dom/11-dom-collections.js
+++ 07-js-html-dom/11-dom-collections.js
@@ -1,3 +1,6 @@
 // The HTMLCollection Object.
 const pCollection = document.getElementsByTagName('p');
 console.log(pCollection.length);
+for (let i = 0; i < pCollection.length; i++) {
+    pCollection[i].style.color = 'red';
+}
