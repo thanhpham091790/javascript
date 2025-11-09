72a3a2387828fadf3de86b6b2ab1b348125bf1d2
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-11-09 08:45:43 -0700

    Creating new HTML elements and insert the element before an existed element.

--- 07-js-html-dom/10-dom-nodes.js
+++ 07-js-html-dom/10-dom-nodes.js
@@ -1,7 +1,11 @@
-// Creating new HTML elements (nodes)
-const para = document.createElement('p');
-const textNode = document.createTextNode('This is a new paragraph!');
-para.appendChild(textNode);
+// Creating new HTML elements - insertBefore()
 
-const div = document.querySelector('#div1');
-div.appendChild(para);
+const div1 = document.querySelector('#div1');
+const p1 = document.querySelector('#p1');
+const p2 = document.querySelector('#p2');
+
+const newPara = document.createElement('p');
+const textNode = document.createTextNode('This is a new paragraph!');
+newPara.appendChild(textNode);
+
+div1.insertBefore(newPara, p2);
