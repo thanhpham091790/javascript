706c3e97132380c95cab5ac51b9543878f097f79
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-11-08 22:00:13 -0700

    A common error in DOM processing is to expect an element node to contain text.

---
+++ 07-js-html-dom/09-dom-navigation.js
@@ -0,0 +1,6 @@
+// Child nodes and node values
+
+const el1 = document.querySelector('#id01');
+const el2 = document.querySelector('#id02');
+
+console.log(el1.innerHTML, el1.firstChild.nodeValue, el1.lastChild.nodeValue, el1.childNodes[0].nodeValue);
