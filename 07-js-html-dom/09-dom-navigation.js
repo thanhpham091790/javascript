692d48308f728c5dff2ab71745bed4b56c88effe
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-11-08 22:26:14 -0700

    The document.body property allows access to the body of the document.

--- 07-js-html-dom/09-dom-navigation.js
+++ 07-js-html-dom/09-dom-navigation.js
@@ -1,6 +1,4 @@
-// Child nodes and node values
+// DOM Root Nodes
 
-const el1 = document.querySelector('#id01');
-const el2 = document.querySelector('#id02');
-
-console.log(el1.innerHTML, el1.firstChild.nodeValue, el1.lastChild.nodeValue, el1.childNodes[0].nodeValue);
+const demoEl = document.querySelector('#demo');
+demoEl.innerHTML = document.body.innerHTML;
