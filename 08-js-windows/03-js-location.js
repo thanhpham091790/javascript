b09e11834b23fff1b34d0e19987de3c7b6014e4c
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-11-13 01:21:49 -0700

    The window.location.assign() method loads a new document.

--- 08-js-windows/03-js-location.js
+++ 08-js-windows/03-js-location.js
@@ -1,8 +1,12 @@
 // Window location href
 const demoEl = document.querySelector('#demo');
+const btn = document.querySelector('#btn');
 demoEl.innerHTML =
     'Page location href: ' + window.location.href + '.<br>' +
     'Page location pathname: ' + window.location.pathname + '.<br>' +
     'Page location protocol: ' + window.location.protocol + '.<br>' +
     'Page location port: ' + window.location.port + '.<br>' +
     'Page location hostname: ' + window.location.hostname;
+btn.addEventListener('click', () => {
+    window.location.assign('https://www.w3schools.com');
+});
