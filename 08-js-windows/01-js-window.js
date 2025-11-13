9eec358817beae9388168e2152d3592f3f01bb2d
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-11-12 21:57:14 -0700

    Two properties can be used to determine the size of the browser window. The browser window is not including toolbars and scrollbars.

---
+++ 08-js-windows/01-js-window.js
@@ -0,0 +1,4 @@
+const demoEl = document.querySelector('#demo');
+demoEl.innerHTML =
+    'Browser inner window width: ' + window.innerWidth + 'px.<br>' +
+    'Browser inner window height: ' + window.innerHeight + 'px.';
