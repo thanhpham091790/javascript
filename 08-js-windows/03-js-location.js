f7b89404d43b9df8c8721958f93ff8fe2d7d48ce
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-11-13 01:19:21 -0700

    The window.location.port property returns the port of the current page.

--- 08-js-windows/03-js-location.js
+++ 08-js-windows/03-js-location.js
@@ -4,4 +4,5 @@ demoEl.innerHTML =
     'Page location href: ' + window.location.href + '.<br>' +
     'Page location pathname: ' + window.location.pathname + '.<br>' +
     'Page location protocol: ' + window.location.protocol + '.<br>' +
+    'Page location port: ' + window.location.port + '.<br>' +
     'Page location hostname: ' + window.location.hostname;
