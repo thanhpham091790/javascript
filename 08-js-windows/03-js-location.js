d3dc79763bc5dbd48b46cfa6391811240214c1b8
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-11-13 01:14:36 -0700

    The window.location.pathname returns the path name of the current page.

--- 08-js-windows/03-js-location.js
+++ 08-js-windows/03-js-location.js
@@ -2,4 +2,5 @@
 const demoEl = document.querySelector('#demo');
 demoEl.innerHTML =
     'Page location href: ' + window.location.href + '.<br>' +
+    'Page location pathname: ' + window.location.pathname + '.<br>' +
     'Page location hostname: ' + window.location.hostname;
