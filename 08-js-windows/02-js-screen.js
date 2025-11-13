6febbc8cb3352668d0ea19b8de27bd96406d700e
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-11-12 22:49:09 -0700

    The screen.availableWidth and screen.availableHeight properties return the width and height of the visitor screen, in pixels, minus interface features like Window Taskbar.

--- 08-js-windows/02-js-screen.js
+++ 08-js-windows/02-js-screen.js
@@ -2,6 +2,8 @@
 const demoEl = document.querySelector('#demo');
 demoEl.innerHTML =
     'Screen width: ' + screen.width + 'px.<br>' +
-    'Window width: ' + window.innerWidth + 'px.<br>' +
     'Screen height: ' + screen.height + 'px.<br>' +
-    'Window height: ' + window.innerHeight + 'px.';
+    'Screen available width: ' + screen.availWidth + 'px.<br>' +
+    'Screen available height: ' + screen.availHeight + 'px.<br>' +
+    'Window width: ' + window.innerWidth + 'px.<br>' +
+    'Window height: ' + window.innerHeight + 'px.<br>';
