e065823ccddaa94d5192e518984e2af44edf368d
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-11-13 21:15:58 -0700

    The appName property returns the application name of the browser.

--- 08-js-windows/05-js-navigator.js
+++ 08-js-windows/05-js-navigator.js
@@ -2,4 +2,6 @@
 const demoEl = document.querySelector('#demo');
 demoEl.innerHTML =
     'The cookiesEnable is ' + navigator.cookieEnabled + '. <br>' +
+    'The browser is online ' + navigator.onLine + '. <br>' +
+    'The browser application name ' + navigator.appName + '. <br>' +
     'The language is ' + navigator.language + '. <br>';
