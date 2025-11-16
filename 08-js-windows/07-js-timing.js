3b4ce149eb177f74026b60fbd13c7f8d319347c6
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-11-15 22:10:09 -0700

    The clearInterval() method stops the excutions of the function specified in the setInterval() method.

--- 08-js-windows/07-js-timing.js
+++ 08-js-windows/07-js-timing.js
@@ -1,4 +1,11 @@
-setInterval(() => {
+
+const button = document.querySelector('button');
+
+let time = setInterval(() => {
     const d = new Date();
     document.querySelector('#demo').innerHTML = d.toLocaleTimeString();
 }, 1000);
+
+button.addEventListener('click', () => {
+    clearInterval(time);
+});
