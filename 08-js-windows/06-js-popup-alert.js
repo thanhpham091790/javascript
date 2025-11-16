9eb4a37a6cc436e3734f3667b4a69101fb19effe
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-11-15 18:22:12 -0700

    A prompt box is often used if you want the user to input a value before entering a page.

--- 08-js-windows/06-js-popup-alert.js
+++ 08-js-windows/06-js-popup-alert.js
@@ -1,14 +1,14 @@
-// Confirm Box
+// Prompt Box
 const btn = document.querySelector('button');
 const demoEl = document.querySelector('#demo');
 
 btn.addEventListener('click', () => {
-    var text;
-    if (confirm('Press a button!')) {
-        text = 'You pressed OK!';
+    let text;
+    let person = prompt('Please enter your name:', 'Thanh Pham');
+    if (person == null || person == '') {
+        text = 'User cancelled the prompt.';
     } else {
-        text = 'You pressed Cancel!';
+        text = 'Hello ' + person + '! How are you today?';
     }
     demoEl.innerHTML = text;
 });
-
