b2a493286a6d7d48749ccae87d705e93ae9b9e4d
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-11-15 18:24:53 -0700

    To display line breaks inside a popup box, use a back-slash followed by the character n.

--- 08-js-windows/06-js-popup-alert.js
+++ 08-js-windows/06-js-popup-alert.js
@@ -3,12 +3,5 @@ const btn = document.querySelector('button');
 const demoEl = document.querySelector('#demo');
 
 btn.addEventListener('click', () => {
-    let text;
-    let person = prompt('Please enter your name:', 'Thanh Pham');
-    if (person == null || person == '') {
-        text = 'User cancelled the prompt.';
-    } else {
-        text = 'Hello ' + person + '! How are you today?';
-    }
-    demoEl.innerHTML = text;
+    alert('Hello \nHow are you ?');
 });
