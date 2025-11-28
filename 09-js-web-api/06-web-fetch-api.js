9d6edb3b3cbe6f4c7591e4434e8de9d1bb179a12
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-11-27 19:47:53 -0700

    Or event better: use understandable names instead of x and y.

--- 09-js-web-api/06-web-fetch-api.js
+++ 09-js-web-api/06-web-fetch-api.js
@@ -1,8 +1,7 @@
 const demoEl = document.querySelector('#demo');
 getText('fetch_info.txt');
-
 async function getText(file) {
-    let x = await fetch(file);
-    let y = await x.text();
-    demoEl.innerHTML = y
+    let myObject = await fetch(file);
+    let myText = await myObject.text();
+    demoEl.innerHTML = myText;
 }
