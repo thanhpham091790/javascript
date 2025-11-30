fb7744a7603f1a5d02c75e2af0b477f0e47d3bb5
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-11-29 21:57:47 -0700

    The getAllResponseHeaders() method returns all header information from the server response.

---
+++ 10-js-ajax/03-ajax-response.js
@@ -0,0 +1,19 @@
+const button = document.querySelector('button');
+const demo = document.querySelector('#demo');
+
+button.addEventListener('click', sendGetRequest);
+
+function sendGetRequest() {
+    const xmlHttp = new XMLHttpRequest();
+    xmlHttp.addEventListener('readystatechange', () => {
+        readFile(xmlHttp);
+    });
+    xmlHttp.open('GET', `ajax-info.txt`, true);
+    xmlHttp.send();
+}
+
+function readFile(xmlHttp) {
+    if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+        demo.innerHTML = xmlHttp.getAllResponseHeaders();
+    }
+}
