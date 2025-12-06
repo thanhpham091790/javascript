feac9cba621d18d98afa2793c2ae714bb7b61297
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-12-06 16:31:30 -0700

    The responseXML property.

--- 10-js-ajax/06-request-header-information.js
+++ 10-js-ajax/06-request-header-information.js
@@ -6,12 +6,28 @@ button.addEventListener('click', sendRequest);
 function sendRequest() {
     const xhr = new XMLHttpRequest();
     xhr.addEventListener('load', () => renderResponse(xhr));
-    xhr.open('GET', 'ajax-info.txt', true);
+    xhr.open('GET', 'cd_catalog.xml', true);
     xhr.send();
 }
 
 function renderResponse(xhr) {
     if (xhr.readyState === 4 && xhr.status === 200) {
-        demo.innerHTML = xhr.responseText;
+        let table =
+            `<table>
+                <tr>
+                    <th>TITLE</th>
+                    <th>ARTISTS</th>
+                <tr>`;
+        const cds = xhr.responseXML.getElementsByTagName('CD');
+        for (const cd of cds) {
+            table +=
+                `<tr>
+                    <td>${cd.getElementsByTagName('TITLE')[0].innerHTML}</td>
+                    <td>${cd.getElementsByTagName('ARTIST')[0].innerHTML}</td>
+                </tr>`;
+        }
+
+        table += `</table>`;
+        demo.innerHTML = table;
     }
 }
