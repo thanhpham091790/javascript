a72155a1565eacaa8bfe5383c73357d59ec73c7a
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-12-06 16:35:38 -0700

    The getAllResponseHeaders() method returns all the header informaiton from the server resource.

--- 10-js-ajax/06-request-header-information.js
+++ 10-js-ajax/06-request-header-information.js
@@ -5,29 +5,11 @@ button.addEventListener('click', sendRequest);
 
 function sendRequest() {
     const xhr = new XMLHttpRequest();
-    xhr.addEventListener('load', () => renderResponse(xhr));
+    xhr.addEventListener('load', () => getHeaderInfo(xhr));
     xhr.open('GET', 'cd_catalog.xml', true);
     xhr.send();
 }
 
-function renderResponse(xhr) {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-        let table =
-            `<table>
-                <tr>
-                    <th>TITLE</th>
-                    <th>ARTISTS</th>
-                <tr>`;
-        const cds = xhr.responseXML.getElementsByTagName('CD');
-        for (const cd of cds) {
-            table +=
-                `<tr>
-                    <td>${cd.getElementsByTagName('TITLE')[0].innerHTML}</td>
-                    <td>${cd.getElementsByTagName('ARTIST')[0].innerHTML}</td>
-                </tr>`;
-        }
-
-        table += `</table>`;
-        demo.innerHTML = table;
-    }
+function getHeaderInfo(xhr) {
+    demo.innerHTML = xhr.getAllResponseHeaders();
 }
