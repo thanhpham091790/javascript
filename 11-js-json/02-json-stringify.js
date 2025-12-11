917e5b0d2e688bfffa57a546f714fd3bd1b71865
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-12-10 21:59:22 -0700

    You can keep your functions if you convert these  functions into strings first and run the JSON.stringify() method later.

--- 11-js-json/02-json-stringify.js
+++ 11-js-json/02-json-stringify.js
@@ -1,4 +1,5 @@
 const demo = document.querySelector('#demo');
 const obj = { name: "John", age: function () { return 30; }, city: "New York" };
+obj.age = obj.age.toString();
 const json = JSON.stringify(obj);
 demo.innerHTML = json;
