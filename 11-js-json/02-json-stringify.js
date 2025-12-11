d64855895a8315d97fe9b5c82b3668840548a68e
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-12-10 19:59:36 -0700

    The JSON.stringify() method converts date objects into things.

--- 11-js-json/02-json-stringify.js
+++ 11-js-json/02-json-stringify.js
@@ -1,11 +1,5 @@
 const demo = document.querySelector('#demo');
 
-// Storing data:
-const myObject = { name: "John", age: 31, city: "New York" };
-const myJSON = JSON.stringify(myObject);
-localStorage.setItem('myJSON', myJSON);
-
-// Retrieving data:
-const json = localStorage.getItem('myJSON');
-const object = JSON.parse(json);
-demo.innerHTML = `Hello, my name is ${object.name}`;
+const obj = { name: "John", today: new Date(), city: "New York" };
+const json = JSON.stringify(obj);
+demo.innerHTML = json;
