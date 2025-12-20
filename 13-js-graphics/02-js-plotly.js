f440bf5921a9a01b477bbcc81b45f98eaab8e50d
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-12-20 14:12:49 -0700

    Plotty pie charts.

--- 13-js-graphics/02-js-plotly.js
+++ 13-js-graphics/02-js-plotly.js
@@ -1,12 +1,10 @@
-const xArray = [55, 49, 44, 24, 15];
-const yArray = ["Italy", "France", "Spain", "USA", "Argentina"];
+const xArray = ["Italy", "France", "Spain", "USA", "Argentina"];
+const yArray = [55, 49, 44, 24, 15];
 
 const data = [{
-    x: xArray,
-    y: yArray,
-    type: 'bar',
-    orientation: 'h',
-    marker: { color: 'salmon' }
+    labels: xArray,
+    values: yArray,
+    type: 'pie',
 }];
 
 const layout = { title: 'Wine Production' };
