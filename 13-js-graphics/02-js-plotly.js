0c6e6985ef20896ba1393cf8913a81d71f449d7d
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-12-20 14:10:36 -0700

    Plotly horizontal bar charts.

--- 13-js-graphics/02-js-plotly.js
+++ 13-js-graphics/02-js-plotly.js
@@ -1,11 +1,11 @@
-const xArray = ["Italy", "France", "Spain", "USA", "Argentina"];
-const yArray = [55, 49, 44, 24, 15];
+const xArray = [55, 49, 44, 24, 15];
+const yArray = ["Italy", "France", "Spain", "USA", "Argentina"];
 
 const data = [{
     x: xArray,
     y: yArray,
     type: 'bar',
-    orientation: 'v',
+    orientation: 'h',
     marker: { color: 'salmon' }
 }];
 
