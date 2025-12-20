72a98c4f5e8ad9e37c1deaaafac5b2da19f91910
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-12-20 13:34:19 -0700

    Line graphs

--- 13-js-graphics/01-js-canvas.js
+++ 13-js-graphics/01-js-canvas.js
@@ -1,16 +1,18 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
+ctx.fillStyle = '#FF0000';
 canvas.height = canvas.width;
 ctx.transform(1, 0, 0, -1, 0, canvas.height)
 
-const xArray = [50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150];
-const yArray = [7, 8, 8, 9, 9, 9, 10, 11, 14, 14, 15];
+let xMax = canvas.height;
+let slope = 1.2;
+let intercept = 70;
 
-ctx.fillStyle = "red";
-for (let i = 0; i < xArray.length - 1; i++) {
-    let x = xArray[i] * 400 / 150;
-    let y = yArray[i] * 400 / 15;
-    ctx.beginPath();
-    ctx.ellipse(x, y, 3, 3, 0, 0, Math.PI * 2);
-    ctx.fill();
+ctx.moveTo(0, intercept);
+ctx.lineTo(xMax, f(xMax));
+ctx.strokeStyle = 'black';
+ctx.stroke();
+
+function f(x) {
+    return x * slope + intercept;
 }
