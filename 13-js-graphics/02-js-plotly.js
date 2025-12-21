d7e2a241810afb3b2b846ba5f6afc24d392b0060
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-12-20 21:20:38 -0700

    Plotly to create scatter plots chart.

--- 13-js-graphics/02-js-plotly.js
+++ 13-js-graphics/02-js-plotly.js
@@ -1,17 +1,18 @@
 
 // Generate values
-const xValues = [];
-const yValues = [];
-for (let i = 0; i <= 10; i += 0.2) {
-    xValues.push(i);
-    yValues.push(Math.cos(i));
-}
+const xArray = [50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150];
+const yArray = [7, 8, 8, 9, 9, 9, 10, 11, 14, 14, 15];
 
 const data = [{
-    x: xValues,
-    y: yValues,
+    x: xArray,
+    y: yArray,
     mode: 'markers'
 }];
 
-const layout = { title: `y = Math.cos(x)` };
+const layout = {
+    xaxis: { range: [40, 160], title: 'Square Meters' },
+    yaxis: { range: [5, 16], title: 'Price in Millions' },
+    title: 'House Prices vs Size'
+};
+
 Plotly.newPlot('myPlot', data, layout);
