aed9bb66d9b4b3e55678205ea159ee664c5987ea
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-12-20 21:45:38 -0700

    Using Plotly library to create linear graph.

--- 13-js-graphics/02-js-plotly.js
+++ 13-js-graphics/02-js-plotly.js
@@ -1,29 +1,12 @@
 
 // Generate values
-const data = [
-    {
-        x: [1, 2, 3, 4],
-        y: [10, 20, 30, 40],
-        mode: 'markers',
-        marker: {
-            color: ['red', 'green', 'blue', 'orange'],
-            size: [20, 30, 40, 50]
-        }
-    },
-    {
-        x: [1, 2, 3, 4],
-        y: [15, 30, 45, 60],
-        mode: 'markers',
-        marker: {
-            color: 'rgb(31, 119, 180)',
-            size: 18,
-            symbol: ['circle', 'square', 'diamond', 'cross']
-        }
-    }
-];
-
-const layout = {
-    title: 'Plotting Bubbles'
-};
+const x = [];
+const y = [];
+for (let i = 0; i <= 10; i++) {
+    x.push(i);
+    y.push(i + 17);
+}
+const data = [{ x, y, mode: 'lines' }];
+const layout = { title: 'y = x + 17' };
 
 Plotly.newPlot('myPlot', data, layout);
