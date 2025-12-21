51c3a211f9f6b6904cc8a8f673900fc9f39458b8
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-12-20 22:05:54 -0700

    Using Plotly library to create multiple lines graphs.

--- 13-js-graphics/02-js-plotly.js
+++ 13-js-graphics/02-js-plotly.js
@@ -1,12 +1,28 @@
 
 // Generate values
-const x = [];
-const y = [];
+const x1 = [];
+const x2 = [];
+const x3 = [];
+const y1 = [];
+const y2 = [];
+const y3 = [];
+
 for (let i = 0; i <= 10; i++) {
-    x.push(i);
-    y.push(i + 17);
+    x1.push(i);
+    x2.push(i);
+    x3.push(i);
+
+    y1.push(i);
+    y2.push(i * 1.5);
+    y3.push(i * 1.5 + 7);
 }
-const data = [{ x, y, mode: 'lines' }];
-const layout = { title: 'y = x + 17' };
+
+
+const data = [
+    { x: x1, y: y1, mode: 'lines' },
+    { x: x2, y: y2, mode: 'lines' },
+    { x: x3, y: y3, mode: 'lines' }
+];
+const layout = { title: `y=x | y=x*1.5 | y=x*1.5+7` };
 
 Plotly.newPlot('myPlot', data, layout);
