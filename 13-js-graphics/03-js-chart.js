9337cf4b8855fa8a5ef349605cfc5c14202a8b95
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-12-22 10:10:42 -0700

    Bar chart - color shades.

--- 13-js-graphics/03-js-chart.js
+++ 13-js-graphics/03-js-chart.js
@@ -1,6 +1,10 @@
 const x = ["Italy", "France", "Spain", "USA", "Argentina"];
 const y = [55, 49, 44, 24, 15];
-const colors = ['salmon'];
+const colors = ["rgba(0,0,255,1.0)",
+    "rgba(0,0,255,0.8)",
+    "rgba(0,0,255,0.6)",
+    "rgba(0,0,255,0.4)",
+    "rgba(0,0,255,0.2)",];
 
 const ctx = document.querySelector('#myChart');
 
