1c156a336f4d7cebee1ffba9922d280bc2545d7f
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-12-22 11:34:58 -0700

    Using Chart.js to create Scatter Plots.

--- 13-js-graphics/03-js-chart.js
+++ 13-js-graphics/03-js-chart.js
@@ -1,28 +1,35 @@
 
 const ctx = document.querySelector('#myChart');
 
-const labels = ["Italy", "France", "Spain", "USA", "Argentina"];
-const backgroundColor = [
-    "#b91d47",
-    "#00aba9",
-    "#2b5797",
-    "#e8c3b9",
-    "#1e7145"
+const type = 'scatter';
+const pointRadius = 4;
+const pointBackgroundColor = 'rgb(0,0,255)';
+const data = [
+    { x: 50, y: 7 },
+    { x: 60, y: 8 },
+    { x: 70, y: 8 },
+    { x: 80, y: 9 },
+    { x: 90, y: 9 },
+    { x: 100, y: 9 },
+    { x: 110, y: 10 },
+    { x: 120, y: 11 },
+    { x: 130, y: 14 },
+    { x: 140, y: 14 },
+    { x: 150, y: 15 }
 ];
-const data = [55, 49, 44, 24, 15];
 const legend = { display: true };
 const title = {
     display: true,
-    text: 'World Wine Production 2018',
+    text: 'House Prices vs. Size',
     font: { size: 16 }
 };
 
 new Chart(ctx, {
-    type: 'doughnut',
+    type,
     data: {
-        labels,
         datasets: [{
-            backgroundColor,
+            pointRadius,
+            pointBackgroundColor,
             data
         }]
     },
