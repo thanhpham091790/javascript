5b796586505aa3080a93e24864f8c83aaf6d5392
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-12-22 11:49:32 -0700

    Using Chart.js to create Multiple Lines chart.

--- 13-js-graphics/03-js-chart.js
+++ 13-js-graphics/03-js-chart.js
@@ -2,13 +2,25 @@
 const ctx = document.querySelector('#myChart');
 
 const type = 'line';
-const labels = [50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150];
-const fill = false;
-const lineTension = 0;
-const backgroundColor = "rgba(0,0,255,1.0)";
-const borderColor = "rgba(0,0,255,0.1)";
-const data = [7, 8, 8, 9, 9, 9, 10, 11, 14, 14, 15];
-
+const labels = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000];
+const datasets = [
+    {
+        data: [860, 1140, 1060, 1060, 1070, 1110, 1330, 2210, 7830, 2478],
+        borderColor: "red",
+        pointRadius: 2,
+        tension: 0.4,
+    }, {
+        data: [1600, 1700, 1700, 1900, 2000, 2700, 4000, 5000, 6000, 7000],
+        borderColor: "green",
+        pointRadius: 2,
+        tension: 0.4,
+    }, {
+        data: [300, 700, 2000, 5000, 6000, 4000, 2000, 1000, 200, 100],
+        borderColor: "blue",
+        pointRadius: 2,
+        tension: 0.4,
+    }
+];
 const legend = { display: false };
 const title = {
     display: true,
@@ -20,13 +32,7 @@ new Chart(ctx, {
     type,
     data: {
         labels,
-        datasets: [{
-            fill: false,
-            lineTension,
-            backgroundColor,
-            borderColor,
-            data
-        }]
+        datasets
     },
     options: {
         plugins: {
