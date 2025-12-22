26c715c6dfab6721be3317c77f49acc11ff2ad65
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-12-22 11:59:19 -0700

    Using Chart.js to create Linear Graphs.

--- 13-js-graphics/03-js-chart.js
+++ 13-js-graphics/03-js-chart.js
@@ -2,31 +2,33 @@
 const ctx = document.querySelector('#myChart');
 
 const type = 'line';
-const labels = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000];
+
+const labels = [];
+const data = [];
+
+for (let i = 0; i <= 10; i += 0.5) {
+    labels.push(i);
+    data.push(i * 2 + 7);
+}
 const datasets = [
     {
-        data: [860, 1140, 1060, 1060, 1070, 1110, 1330, 2210, 7830, 2478],
-        borderColor: "red",
-        pointRadius: 2,
-        tension: 0.4,
-    }, {
-        data: [1600, 1700, 1700, 1900, 2000, 2700, 4000, 5000, 6000, 7000],
-        borderColor: "green",
-        pointRadius: 2,
-        tension: 0.4,
-    }, {
-        data: [300, 700, 2000, 5000, 6000, 4000, 2000, 1000, 200, 100],
-        borderColor: "blue",
-        pointRadius: 2,
-        tension: 0.4,
+        fill: false,
+        pointRadius: 1,
+        borderColor: "rgba(255,0,0,0.5)",
+        data
     }
 ];
 const legend = { display: false };
 const title = {
     display: true,
-    text: 'House Prices vs. Size',
+    text: 'y = x * 2 + 7',
     font: { size: 16 }
 };
+
+const scales = {
+    x: { title: { display: true, text: 'x' } },
+    y: { title: { display: true, text: 'sin(x)' } }
+}
 
 new Chart(ctx, {
     type,
@@ -37,7 +39,8 @@ new Chart(ctx, {
     options: {
         plugins: {
             legend,
-            title
+            title,
+            scales
         }
     }
 });
