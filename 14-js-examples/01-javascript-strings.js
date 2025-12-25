50784ee6bc70050a02a8e07a3331f26d5f7c8100
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-12-25 12:40:23 -0700

    Replace characters in a string - replace()

--- 14-js-examples/01-javascript-strings.js
+++ 14-js-examples/01-javascript-strings.js
@@ -4,7 +4,6 @@ const demo = document.querySelector('#demo');
 button.addEventListener('click', myFunc);
 
 function myFunc() {
-    var str = "The rain in SPAIN stays mainly in the plain";
-    var res = str.match(/ain/g);
-    demo.innerHTML = res;
+    let text = demo.innerHTML;
+    demo.innerHTML = text.replace("Microsoft", "W3Schools");
 }
