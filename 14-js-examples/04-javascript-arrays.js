942379302db104e80f3f441fd6561e86152d0cae
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-12-27 19:46:00 -0700

    Javascript array iteration Array.forEach() method.

--- 14-js-examples/04-javascript-arrays.js
+++ 14-js-examples/04-javascript-arrays.js
@@ -1,35 +1,10 @@
 
 const demo = document.querySelector('#demo');
-const button = document.querySelector('button');
 
-const cars = [
-    { type: "Volvo", year: 2016 },
-    { type: "Saab", year: 2001 },
-    { type: "BMW", year: 2010 }
-];
+const numbers = [45, 4, 9, 16, 25];
+let text = '';
+numbers.forEach(function (value) {
+    text += value + '<br>';
+});
 
-displayCars();
-
-function displayCars() {
-    demo.innerHTML =
-        cars[0].type + ' ' + cars[0].year + '<br>' +
-        cars[1].type + ' ' + cars[1].year + '<br>' +
-        cars[2].type + ' ' + cars[2].year;
-}
-
-button.addEventListener('click', sortObjectByNumber);
-
-function sortObjectByNumber() {
-    cars.sort(function (a, b) {
-        let x = a.type.toLocaleLowerCase();
-        let y = b.type.toLocaleLowerCase();
-        if (x < y) {
-            return -1;
-        }
-        if (x > y) {
-            return 1;
-        }
-        return 0;
-    });
-    displayCars();
-}
+demo.innerHTML = text;
