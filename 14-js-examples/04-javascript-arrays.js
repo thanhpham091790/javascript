b24bc3513e908d9262c25abbf38a0e4892e6a956
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-12-27 20:05:08 -0700

    Javascript array iteration: Array.some() method.

--- 14-js-examples/04-javascript-arrays.js
+++ 14-js-examples/04-javascript-arrays.js
@@ -3,8 +3,8 @@ const demo = document.querySelector('#demo');
 
 const numbers = [45, 4, 9, 19, 25];
 
-let allOver18 = numbers.every(function (number) {
+let someOver18 = numbers.some(function (number) {
     return number > 18;
 });
 
-demo.innerHTML = `All over 18 is ${allOver18}`;
+demo.innerHTML = `All over 18 is ${someOver18}`;
