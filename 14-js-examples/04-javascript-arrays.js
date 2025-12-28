58449435187d1da26c8bbe476226bc57b83b5f1c
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-12-27 20:17:13 -0700

    Javascript array iteration: Array.lastIndexOf() method.

--- 14-js-examples/04-javascript-arrays.js
+++ 14-js-examples/04-javascript-arrays.js
@@ -3,6 +3,6 @@ const demo = document.querySelector('#demo');
 
 const fruits = ["Apple", "Orange", "Apple", "Mango"];
 
-let position = fruits.indexOf('Apple') + 1;
+let position = fruits.lastIndexOf('Apple') + 1;
 
 demo.innerHTML = `Apple is found in position ${position}.`;
