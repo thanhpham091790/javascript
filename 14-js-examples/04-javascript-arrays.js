b52bd97b95e3679a1ceb7a5995125dc0f9333a26
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-12-27 19:57:01 -0700

    Javascript array iteration: Array.reduce().

--- 14-js-examples/04-javascript-arrays.js
+++ 14-js-examples/04-javascript-arrays.js
@@ -2,8 +2,9 @@
 const demo = document.querySelector('#demo');
 
 const numbers = [45, 4, 9, 19, 25];
-const over18 = numbers.filter(function (value) {
-    return value > 18;
+
+let sum = numbers.reduce(function (total, value) {
+    return total + value;
 });
 
-demo.innerHTML = over18;
+demo.innerHTML = sum;
