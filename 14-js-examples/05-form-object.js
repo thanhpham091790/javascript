742be76a00fc2028de11316ba7dc971ba054a5c5
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-12-29 16:29:47 -0700

    Get accept-charset of the form.

--- 14-js-examples/05-form-object.js
+++ 14-js-examples/05-form-object.js
@@ -4,12 +4,7 @@ const submit = document.querySelector('#submitBtn');
 const reset = document.querySelector('#resetBtn');
 
 submit.addEventListener('click', function () {
-    let dataObj = {};
-    console.log(form);
-    for (let i = 0; i < form.length; i++) {
-        dataObj[form.elements[i].name] = form.elements[i].value;
-    }
-    console.log(dataObj);
+    console.log(form.acceptCharset);
 });
 
 reset.addEventListener('click', function () {
