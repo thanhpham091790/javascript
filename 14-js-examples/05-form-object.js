c92a655fa68f9cac2d781d922de8d832940fcc72
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-12-29 16:02:25 -0700

    Submit a form.

---
+++ 14-js-examples/05-form-object.js
@@ -0,0 +1,6 @@
+
+const form = document.querySelector('#frm1');
+const button = document.querySelector('input[type=button]');
+button.addEventListener('click', function () {
+    form.submit();
+});
