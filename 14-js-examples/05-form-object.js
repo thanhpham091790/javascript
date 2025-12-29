b2efd5b255e74678ad4864a228fe5e30e4cf4670
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-12-29 16:38:23 -0700

    Find the method for sending form data.

--- 14-js-examples/05-form-object.js
+++ 14-js-examples/05-form-object.js
@@ -4,7 +4,7 @@ const submit = document.querySelector('#submitBtn');
 const reset = document.querySelector('#resetBtn');
 
 submit.addEventListener('click', function () {
-    console.log(form.length);
+    console.log(form.method);
 });
 
 reset.addEventListener('click', function () {
