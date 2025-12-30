b697ba7dfbd22ae370b6ba08ef47a5b77360158a
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-12-29 17:15:07 -0700

    Select multiple options in a dropdown list.

--- 14-js-examples/06-option-and-select-object.js
+++ 14-js-examples/06-option-and-select-object.js
@@ -3,13 +3,21 @@ const enable = document.querySelector('#enable');
 const mySelect = document.querySelector('#mySelect');
 const demo = document.querySelector('#demo');
 const changeSize = document.querySelector('#changeSize');
+const multiple = document.querySelector('#multiple');
 
 disable.addEventListener('click', disableHandler);
 enable.addEventListener('click', enableHandler);
 changeSize.addEventListener('click', changeSizeHandler);
+multiple.addEventListener('click', multipleHandler);
+
+function multipleHandler() {
+    changeSizeHandler();
+    mySelect.multiple = true;
+}
 
 function changeSizeHandler() {
     mySelect.size = 4;
+
 }
 
 function disableHandler() {
