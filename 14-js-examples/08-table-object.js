4762d32f9ab78bdf82bed3c8e6e8235172e0492c
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-01-17 14:14:43 -0700

    Delete rows in a table.

--- 14-js-examples/08-table-object.js
+++ 14-js-examples/08-table-object.js
@@ -2,9 +2,8 @@ const myTable = document.querySelector('#myTable');
 const button = document.querySelector('button');
 const demo = document.querySelector('#demo');
 
-button.addEventListener('click', createCaption);
+button.addEventListener('click', deleteOneRow);
 
-function createCaption() {
-    console.log(myTable);
-    myTable.createCaption().innerHTML = 'Table Caption';
+function deleteOneRow() {
+    myTable.deleteRow(0);
 }
