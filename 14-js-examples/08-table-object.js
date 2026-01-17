fb2f997dcacd4efc6f07ee80b2a91c044fc82772
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-01-17 14:18:14 -0700

    Add new row to table.

--- 14-js-examples/08-table-object.js
+++ 14-js-examples/08-table-object.js
@@ -2,8 +2,11 @@ const myTable = document.querySelector('#myTable');
 const button = document.querySelector('button');
 const demo = document.querySelector('#demo');
 
-button.addEventListener('click', deleteOneRow);
+button.addEventListener('click', insertRow);
 
-function deleteOneRow() {
-    myTable.deleteRow(0);
+function insertRow() {
+    let row = myTable.insertRow(0);
+    let cell0 = row.insertCell(0);
+    let cell1 = row.insertCell(1);
+    cell0.innerHTML = cell1.innerHTML = 'New';
 }
