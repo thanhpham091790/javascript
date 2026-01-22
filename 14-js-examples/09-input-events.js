82bd43069de83b156dc52c6e0ac696031d0e5705
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-01-22 11:01:13 -0700

    onselect - when input text is selected.

--- 14-js-examples/09-input-events.js
+++ 14-js-examples/09-input-events.js
@@ -1,7 +1,19 @@
-const name = document.querySelector('#name');
+const input = document.querySelector('input');
+const demo = document.querySelector('#demo');
 
-name.addEventListener('focus', onFocusHandler);
+input.addEventListener('select', onSelectHandler);
 
-function onFocusHandler(event) {
-    event.target.style.backgroundColor = 'yellow';
+function onSelectHandler() {
+    const selectedText = getHighlightedText();
+    if (selectedText.length > 0)
+        demo.textContent = `You selected: ${selectedText}`;
+}
+
+function getHighlightedText() {
+    let text = "";
+    if (window.getSelection) { // Check for the standard method
+        text = window.getSelection().toString();
+    }
+    /* The `document.selection` approach below is for older IE versions and is no longer needed in modern browsers. */
+    return text;
 }
