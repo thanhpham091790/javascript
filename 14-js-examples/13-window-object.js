366f2c3f917d66dfed45f7865f41891fdc289df0
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-01-28 21:54:19 -0700

    Checks whether the new window has been closed or not.

--- 14-js-examples/13-window-object.js
+++ 14-js-examples/13-window-object.js
@@ -1,14 +1,31 @@
-const openWin = document.querySelector('#open');
-const closeWin = document.querySelector('#close');
+const openWin = document.querySelector('#openWin');
+const closeWin = document.querySelector('#closeWin');
+const checkWin = document.querySelector('#checkWin');
+const message = document.querySelector('#msg');
 var myWindow;
 
 openWin.addEventListener('click', openWinHandler);
 closeWin.addEventListener('click', closeWinHandler);
+checkWin.addEventListener('click', checkWinHandler);
 
 
 function openWinHandler() {
     myWindow = window.open('https://www.woot.com/', '_blank', 'width=400,height=400');
 }
 function closeWinHandler() {
-    myWindow.close();
+    if (myWindow)
+        myWindow.close();
+}
+function checkWinHandler() {
+    msg = '';
+    if (!myWindow) {
+        msg = 'was never opened';
+    } else {
+        if (myWindow.closed) {
+            msg = 'is closed';
+        } else {
+            msg = 'is open';
+        }
+    }
+    message.innerHTML = 'myWindow ' + msg;
 }
