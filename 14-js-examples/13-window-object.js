c4f8c9f04a0d3fb001b82ce568b341bb72caeaa9
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-01-28 22:04:46 -0700

    Write some text to the source (parent) window.

--- 14-js-examples/13-window-object.js
+++ 14-js-examples/13-window-object.js
@@ -1,31 +1,8 @@
 const openWin = document.querySelector('#openWin');
-const closeWin = document.querySelector('#closeWin');
-const checkWin = document.querySelector('#checkWin');
-const message = document.querySelector('#msg');
-var myWindow;
 
 openWin.addEventListener('click', openWinHandler);
-closeWin.addEventListener('click', closeWinHandler);
-checkWin.addEventListener('click', checkWinHandler);
-
 
 function openWinHandler() {
-    myWindow = window.open('https://www.woot.com/', '_blank', 'width=400,height=400');
-}
-function closeWinHandler() {
-    if (myWindow)
-        myWindow.close();
-}
-function checkWinHandler() {
-    msg = '';
-    if (!myWindow) {
-        msg = 'was never opened';
-    } else {
-        if (myWindow.closed) {
-            msg = 'is closed';
-        } else {
-            msg = 'is open';
-        }
-    }
-    message.innerHTML = 'myWindow ' + msg;
+    const myWindow = window.open('', '', 'width=400,height=400');
+    myWindow.opener.document.querySelector('#demo').innerHTML = 'A new window has been opened.';
 }
