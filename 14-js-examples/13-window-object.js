7d85d80edc7974198f01c0c005e86d648f1b4386
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-01-28 22:55:46 -0700

    Resize a window by the specified pixels.

--- 14-js-examples/13-window-object.js
+++ 14-js-examples/13-window-object.js
@@ -1,7 +1,16 @@
-const input = document.querySelector('#printPage');
+const openWin = document.querySelector('#openWin');
+const resizeWin = document.querySelector('#resizeWin');
+var myWindow;
 
-input.addEventListener('click', printPage);
+openWin.addEventListener('click', openWinHandler);
+resizeWin.addEventListener('click', resizeWinHandler);
 
-function printPage() {
-    window.print();
+function openWinHandler() {
+    myWindow = window.open('', '', 'width=400,height=400');
+    myWindow.focus();
+}
+
+function resizeWinHandler() {
+    myWindow.resizeBy(50, 50);
+    myWindow.focus();
 }
