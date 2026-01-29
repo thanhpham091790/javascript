6fbdf5319b04771d00d841beac282f42ecf978ba
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-01-28 21:28:07 -0700

    Close the new window.

--- 14-js-examples/13-window-object.js
+++ 14-js-examples/13-window-object.js
@@ -1,20 +1,14 @@
 const openWin = document.querySelector('#open');
-const blurWin = document.querySelector('#blur');
-const focusWin = document.querySelector('#focus');
+const closeWin = document.querySelector('#close');
 var myWindow;
 
 openWin.addEventListener('click', openWinHandler);
-blurWin.addEventListener('click', blurWinHandler);
-focusWin.addEventListener('click', focusWinHandler);
+closeWin.addEventListener('click', closeWinHandler);
+
 
 function openWinHandler() {
     myWindow = window.open('https://www.woot.com/', '_blank', 'width=400,height=400');
-    myWindow.blur();
 }
-function blurWinHandler() {
-    myWindow.blur();
+function closeWinHandler() {
+    myWindow.close();
 }
-function focusWinHandler() {
-    myWindow.focus();
-}
-
