0c56641a2911cda01fa83dd94b4f27af63ba708b
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-02-02 23:47:36 -0700

    Display a confirm box.

--- 14-js-examples/18-popup-boxes.js
+++ 14-js-examples/18-popup-boxes.js
@@ -1,3 +1,9 @@
 function myFunction() {
-    alert('Pham Hong Thanh\nHow are you?');
+    var txt;
+    if (confirm('Press a button')) {
+        txt = 'You pressed OK!';
+    } else {
+        txt = 'You pressed Cancel!';
+    }
+    document.querySelector('#demo').innerHTML = txt;
 }
