90726684208b333a63fa580069b3c9ce43bfc6db
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-02-03 00:34:59 -0700

    Another simple timing.

--- 14-js-examples/19-timing.js
+++ 14-js-examples/19-timing.js
@@ -1,3 +1,27 @@
-function myFunction() {
-    alert('Thanh Pham');
+const demo = document.querySelector('#demo');
+
+
+setTimeout(() => {
+    myTimeout1();
+}, 2000);
+
+setTimeout(() => {
+    myTimeout2();
+}, 4000);
+
+setTimeout(() => {
+    myTimeout3();
+}, 6000);
+
+
+function myTimeout1() {
+    demo.innerHTML = '2 seconds';
+}
+
+function myTimeout2() {
+    demo.innerHTML = '4 seconds';
+}
+
+function myTimeout3() {
+    demo.innerHTML = '6 seconds';
 }
