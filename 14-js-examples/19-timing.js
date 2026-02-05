b0806a88a60204087b8cdc90d70463670e61e502
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-02-05 01:37:29 -0700

    A clock created with a timing event.

--- 14-js-examples/19-timing.js
+++ 14-js-examples/19-timing.js
@@ -1,16 +1,24 @@
-const demo = document.querySelector('#demo');
-const start = document.querySelector('#start');
-const stop = document.querySelector('#stop');
-var count = 0;
+const txt = document.querySelector('#txt');
+window.addEventListener('load', startTime);
 
-start.addEventListener('click', () => {
-    myTimer = setInterval(myCounter, 1000);
-});
 
-stop.addEventListener('click', () => {
-    clearInterval(myTimer);
-});
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    txt.innerHTML = `${h} : ${m} : ${s}`;
+    setTimeout(startTime, 1000);
 
-function myCounter() {
-    demo.innerHTML = ++count;
+    console.log(today);
+}
+
+
+function checkTime(i) {
+    if (i < 10) {
+        i = '0' + i;
+    }
+    return i;
 }
