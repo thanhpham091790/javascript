c19de209ce580799ebcb30f1fc04d4cc6d5a08c5
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-02-05 01:18:08 -0700

    Timing event in an infinity loop - with a Stop butotn.

--- 14-js-examples/19-timing.js
+++ 14-js-examples/19-timing.js
@@ -1,5 +1,16 @@
 const demo = document.querySelector('#demo');
-var c = 0;
+const start = document.querySelector('#start');
+const stop = document.querySelector('#stop');
+var count = 0;
+
+start.addEventListener('click', () => {
+    myTimer = setInterval(myCounter, 1000);
+});
+
+stop.addEventListener('click', () => {
+    clearInterval(myTimer);
+});
+
 function myCounter() {
-    demo.innerHTML = ++c;
+    demo.innerHTML = ++count;
 }
