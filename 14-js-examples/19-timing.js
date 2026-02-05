eebb0401db817cd1cd499a0125675e8e418ade75
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-02-05 01:11:25 -0700

    Timing event in an infinity loop.

--- 14-js-examples/19-timing.js
+++ 14-js-examples/19-timing.js
@@ -1,27 +1,5 @@
 const demo = document.querySelector('#demo');
-
-
-setTimeout(() => {
-    myTimeout1();
-}, 2000);
-
-setTimeout(() => {
-    myTimeout2();
-}, 4000);
-
-setTimeout(() => {
-    myTimeout3();
-}, 6000);
-
-
-function myTimeout1() {
-    demo.innerHTML = '2 seconds';
-}
-
-function myTimeout2() {
-    demo.innerHTML = '4 seconds';
-}
-
-function myTimeout3() {
-    demo.innerHTML = '6 seconds';
+var c = 0;
+function myCounter() {
+    demo.innerHTML = ++c;
 }
